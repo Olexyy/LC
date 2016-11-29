aaa9a9171ca270a5f0ab128d745da6fcfdbddd48
Olexyy <inua@ua.fm>
2016-11-30 00:02:32 +0200

    Changed views, altered layout, created partial, bootstrap and jQuery are local

--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,5 +1,5 @@
 class Subcategory < ApplicationRecord
   belongs_to :category
-  validates_presence_of :name, :message => "Field is required"
+  validates_presence_of :name, :message => 'Field is required'
   validates_numericality_of :weight
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
    get '', to: 'home#index'
    resources :categories
    resources :subcategories
+   get ':controller(/:action(/:id))'
 end
