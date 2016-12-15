18c3782f3de4bba23b41116251b41b7b5e40130b
Olexyy <inua@ua.fm>
2016-12-15 20:28:22 +0200

    Added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+# rake db:seed:dump
 User.create!([
   {email: "admin@example.com", encrypted_password: "$2a$11$KLfce..AEjFH4qmHOuPQQ.HbZUeEN1cTSQbt4eZsb0G1l0eBCp25u", reset_password_token: nil, reset_password_sent_at: nil, remember_created_at: "2016-12-12 23:00:14", sign_in_count: 9, current_sign_in_at: "2016-12-13 18:50:11", last_sign_in_at: "2016-12-12 23:00:14", current_sign_in_ip: "127.0.0.1", last_sign_in_ip: "127.0.0.1"}
 ])
