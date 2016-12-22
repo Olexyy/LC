db57c0eab09abe99088d1a61b0389804ff9d72d9
Olexyy <inua@ua.fm>
2016-12-22 19:29:12 +0200

    toggle onclick implementation file

---
+++ app/assets/javascripts/toggle-onclick.js
@@ -0,0 +1,18 @@
+﻿$(document).on ('ready page:load', function () {
+    if($('ul.collapsible1').length){
+        $('ul.collapsible1').hide();
+        $('ul.collapsible2').hide();
+        $('ul.toggle1 li').on('click', function () {
+            $(this).find('ul.collapsible1').each(function () {
+                $(this).toggle();
+            });
+        });
+        $('ul.toggle2 li').on('click', function (e) {
+            $(this).parent().stop();
+            $(this).find('ul.collapsible2').each(function () {
+                $(this).toggle();
+            });
+            e.stopPropagation();
+        });
+    }
+});
