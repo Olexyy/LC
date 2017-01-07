190dc5a24b23798e328370e0389b1ffc4654be3f
Alex <inua@ua.fm>
2017-01-07 19:41:08 +0000

    Implementation of CRUD for block_fields

--- app/controllers/block_fields_controller.rb
+++ app/controllers/block_fields_controller.rb
@@ -3,13 +3,11 @@ class BlockFieldsController < ApplicationController
   before_action :set_block_field, only: [:show, :edit, :update, :destroy]
 
   # GET /block_fields
-  # GET /block_fields.json
   def index
     @block_fields = BlockField.all
   end
 
   # GET /block_fields/1
-  # GET /block_fields/1.json
   def show
   end
 
@@ -23,42 +21,34 @@ class BlockFieldsController < ApplicationController
   end
 
   # POST /block_fields
-  # POST /block_fields.json
   def create
     @block_field = BlockField.new(block_field_params)
 
     respond_to do |format|
       if @block_field.save
-        format.html { redirect_to @block_field, notice: 'Block field was successfully created.' }
-        format.json { render :show, status: :created, location: @block_field }
+        format.html { redirect_to @block_field, notice: t(:operation_successful) }
       else
         format.html { render :new }
-        format.json { render json: @block_field.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /block_fields/1
-  # PATCH/PUT /block_fields/1.json
   def update
     respond_to do |format|
       if @block_field.update(block_field_params)
-        format.html { redirect_to @block_field, notice: 'Block field was successfully updated.' }
-        format.json { render :show, status: :ok, location: @block_field }
+        format.html { redirect_to @block_field, notice: t(:operation_successful) }
       else
         format.html { render :edit }
-        format.json { render json: @block_field.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /block_fields/1
-  # DELETE /block_fields/1.json
   def destroy
     @block_field.destroy
     respond_to do |format|
-      format.html { redirect_to block_fields_url, notice: 'Block field was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to block_fields_url, notice: t(:operation_successful) }
     end
   end
 
--- app/models/block_field.rb
+++ app/models/block_field.rb
@@ -3,14 +3,17 @@ class BlockField < ApplicationRecord
   validates_presence_of :text
   validates_presence_of :marker
   validates_numericality_of :weight
-  validates_numericality_of :data_type #BOOL conditional
-  #validates :size, inclusion: { in: sizes.keys }
+  # validates :data_type, inclusion: { in: data_types }
   # for conditional blocks! ref to block that depends on field!
   has_many :blocks
   has_many :block_field_block_parts
   has_many :block_parts, through: :block_field_block_parts
-  # todo: translate
-  enum data_type: [:text, :number, :money, :condition]
+
+  enum data_type: [I18n.t(:text), I18n.t(:number), I18n.t(:money), I18n.t(:condition)]
+
+  def self.data_types_list
+    self.data_types.keys.to_a
+  end
 
   def self.sorted
     self.all.sort_by { |i| i.weight }
